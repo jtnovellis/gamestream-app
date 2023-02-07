65ec71ab19d0eb5350d4a35782252c907a1e0a2a
Jairo Jair Toro Novellis <jtnovellis88@gmail.com>
2023-02-07 17:31:03 -0500

    Initial Commit

---
+++ GameStream/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GameStream
+//
+//  Created by Jairo Jair Toro Novellis on 7/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GameStream/GameStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameStreamApp.swift
+//  GameStream
+//
+//  Created by Jairo Jair Toro Novellis on 7/02/23.
+//
+
+import SwiftUI
+
+@main
+struct GameStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
