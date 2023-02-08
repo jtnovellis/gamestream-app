68ff715964253ea04bde64dc9c8ff0af2d2650d7
Jairo Jair Toro Novellis <jtnovellis88@gmail.com>
2023-02-07 20:27:43 -0500

    feat: add register view and homescreen

--- GameStream/ContentView.swift
+++ GameStream/ContentView.swift
@@ -9,11 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            Color(red: 21/255, green: 31/255, blue: 59/255, opacity: 1.0).ignoresSafeArea()
-            VStack {
-                Image("appLogo").resizable().aspectRatio(contentMode: .fit).frame(width: 250).padding(.bottom, 50)
-                StartAndRegisterView()
+        NavigationStack {
+            ZStack {
+                Color(red: 21/255, green: 31/255, blue: 59/255, opacity: 1.0).ignoresSafeArea()
+                VStack {
+                    Image("appLogo").resizable().aspectRatio(contentMode: .fit).frame(width: 250).padding(.bottom, 50)
+                    StartAndRegisterView()
+                }
             }
         }
     }
@@ -48,6 +50,11 @@ struct StartAndRegisterView: View {
 struct LoginView: View {
     @State var email = ""
     @State var password = ""
+    @State var isHomeViewActive = false
+    
+    func login() {
+        self.isHomeViewActive = true
+    }
     
     var body: some View {
         ScrollView {
@@ -57,8 +64,7 @@ struct LoginView: View {
                     if(email.isEmpty) {
                         Text("example@host .com").font(.caption).foregroundColor(.gray)
                     }
-                    
-                    TextField("", text: $email)
+                    TextField("", text: $email).foregroundColor(.white)
                 }
                 Divider().frame(height: 1).background(Color("darkCyan")).padding(.bottom)
                 
@@ -67,15 +73,16 @@ struct LoginView: View {
                     if(password.isEmpty) {
                         Text("*******").font(.caption).foregroundColor(/*@START_MENU_TOKEN@*/.gray/*@END_MENU_TOKEN@*/)
                     }
-                    
-                    SecureField("", text: $password)
+                    SecureField("", text: $password).foregroundColor(.white)
                 }
                 Divider().frame(height: 1).background(Color("darkCyan")).padding(.bottom)
-                Text("¿Olvidaste tu contraseña?").foregroundColor(Color("darkCyan")).font(.footnote).frame(width: 300, alignment: .trailing).padding(.bottom)
+                Text("¿Olvidaste tu contraseña?").foregroundColor(Color("darkCyan")).font(.footnote).frame(width: 300, alignment: .trailing).padding(.bottom, 60)
                 Button {
                     login()
                 } label: {
                     Text("INICIA SESIÓN").fontWeight(.bold).foregroundColor(.white).frame(maxWidth: .infinity, alignment: .center).padding(EdgeInsets(top: 11, leading: 18, bottom: 11, trailing: 18)).overlay(RoundedRectangle(cornerRadius: 6.0).stroke(Color("darkCyan"), lineWidth: 1.5).shadow(color:.white, radius: 5))
+                }.navigationDestination(isPresented: $isHomeViewActive) {
+                    HomeView()
                 }
 
             }.padding(.bottom, 70)
@@ -98,19 +105,91 @@ struct LoginView: View {
     }
 }
 
-func login() {
-    print("login")
-}
 
 struct RegisterView: View {
-    var body: some View {
-        Text("RegisterScreen")
+    @State var email = ""
+    @State var password = ""
+    @State var confirmedPassword = ""
+    @State var isHomeViewActive = false
+    
+    func register() {
+        self.isHomeViewActive = true
     }
+    
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .center) {
+                Text("Elige una foto de perfil").foregroundColor(.white).fontWeight(.bold)
+                Text("Puedes cambiar o elegir más adelante").font(.footnote).foregroundColor(.gray).fontWeight(.light).padding(.bottom)
+                Button {
+                    takeAPick()
+                } label: {
+                    ZStack {
+                        Image("perfilEjemplo").resizable().aspectRatio(contentMode: .fit).frame(width: 80, height: 80)
+                        Image(systemName: "camera").foregroundColor(.white)
+                    }
+                }
+
+            }.padding(.bottom, 40)
+            VStack(alignment: .leading) {
+                Text("Correo electrónico*").foregroundColor(Color("darkCyan"))
+                ZStack(alignment: .leading) {
+                    if(email.isEmpty) {
+                        Text("example@host .com").font(.caption).foregroundColor(.gray)
+                    }
+                    TextField("", text: $email).foregroundColor(.white)
+                }
+                Divider().frame(height: 1).background(Color("darkCyan")).padding(.bottom)
+                Text("Contraseña*").foregroundColor(Color("darkCyan"))
+                ZStack(alignment: .leading) {
+                    if(password.isEmpty) {
+                        Text("*******").font(.caption).foregroundColor(/*@START_MENU_TOKEN@*/.gray/*@END_MENU_TOKEN@*/)
+                    }
+                    SecureField("", text: $password).foregroundColor(.white)
+                }
+                Divider().frame(height: 1).background(Color("darkCyan")).padding(.bottom)
+                Text("Confimar contraseńa*").foregroundColor(Color("darkCyan"))
+                ZStack(alignment: .leading) {
+                    if(confirmedPassword.isEmpty) {
+                        Text("*******").font(.caption).foregroundColor(/*@START_MENU_TOKEN@*/.gray/*@END_MENU_TOKEN@*/)
+                    }
+                    SecureField("", text: $confirmedPassword).foregroundColor(.white)
+                }
+                Divider().frame(height: 1).background(Color("darkCyan")).padding(.bottom, 60)
+                Button {
+                    register()
+                } label: {
+                    Text("REGISTRATE").fontWeight(.bold).foregroundColor(.white).frame(maxWidth: .infinity, alignment: .center).padding(EdgeInsets(top: 11, leading: 18, bottom: 11, trailing: 18)).overlay(RoundedRectangle(cornerRadius: 6.0).stroke(Color("darkCyan"), lineWidth: 1.5).shadow(color:.white, radius: 5))
+                }.navigationDestination(isPresented: $isHomeViewActive) {
+                    HomeView()
+                }
+            }.padding(.bottom, 70)
+            VStack {
+                Text("Registrate con redes sociales").foregroundColor(.white).frame(maxWidth: .infinity, alignment: .center).padding(.bottom)
+                HStack {
+                    Button {
+                        print("login with facebook")
+                    } label: {
+                        Text("Facebook").fontWeight(.bold).foregroundColor(.white).frame(maxWidth: .infinity).padding(EdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 20)).background(Color(red: 40/255, green: 55/255, blue: 88/255)).clipShape(RoundedRectangle(cornerRadius: 10))
+                    }
+                    Button {
+                        print("login with twitter")
+                    } label: {
+                        Text("Twitter").fontWeight(.bold).foregroundColor(.white).frame(maxWidth: .infinity).padding(EdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 20)).background(Color(red: 40/255, green: 55/255, blue: 88/255)).clipShape(RoundedRectangle(cornerRadius: 10))
+                    }
+                }
+            }
+        }.padding(.horizontal, 50.0)
+    }
+}
+
+func takeAPick() {
+    print("taking a pick")
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-        Image("pantalla1")
+        Image("pantalla2")
     }
 }
---
+++ GameStream/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  GameStream
+//
+//  Created by Jairo Jair Toro Novellis on 7/02/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("pantallahome")
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
